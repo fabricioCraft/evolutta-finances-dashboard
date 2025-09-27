6c7f88cb49a6cd243706764447d5023c31668a63
Fabricio <fabricio1991_05@hotmail.com>
2025-09-27 18:46:18 -0300

    feat: add TransactionsService with business logic layer
    - Generate TransactionsService using NestJS CLI
    - Inject PrismaTransactionsRepository in constructor
    - Implement findById method that delegates to repository
    - Implement updateCategory method that delegates to repository
    - Add PrismaTransactionsRepository as provider in TransactionsModule
    - Complete service layer architecture for transactions

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
 import { TransactionProcessingModule } from '../transaction-processing/transaction-processing.module';
+import { TransactionsService } from './transactions.service';
+import { PrismaTransactionsRepository } from './transactions.repository';
 
 @Module({
   imports: [TransactionProcessingModule],
-  controllers: [TransactionsController]
+  controllers: [TransactionsController],
+  providers: [TransactionsService, PrismaTransactionsRepository]
 })
 export class TransactionsModule {}
---
+++ src/transactions/transactions.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaTransactionsRepository } from './transactions.repository';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    private readonly transactionsRepository: PrismaTransactionsRepository,
+  ) {}
+
+  async findById(id: string) {
+    return this.transactionsRepository.findById(id);
+  }
+
+  async updateCategory(id: string, categoryId: string) {
+    return this.transactionsRepository.updateCategory(id, categoryId);
+  }
+}
