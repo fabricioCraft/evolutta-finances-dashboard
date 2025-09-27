7b20475a50053e78bcf20443e8a4a036dc3015fe
Fabricio <fabricio1991_05@hotmail.com>
2025-09-27 19:54:18 -0300

    feat: Criar módulo de gerenciamento de categorias
    - Gerar CategoriesModule usando Nest CLI
    - Criar CategoriesController com rota /categories
    - Criar CategoriesService para lógica de negócio
    - Adicionar testes para controller e service
    - Integrar CategoriesModule ao AppModule
    - Estrutura básica pronta para implementação de CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma.module';
 import { TransactionProcessingModule } from './transaction-processing/transaction-processing.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, TransactionProcessingModule, TransactionsModule],
+  imports: [PrismaModule, TransactionProcessingModule, TransactionsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
