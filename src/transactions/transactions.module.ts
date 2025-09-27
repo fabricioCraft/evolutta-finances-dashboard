5f597a9831798fc704dc92261f148dfc99ceb3ab
Fabricio <fabricio1991_05@hotmail.com>
2025-09-27 18:50:49 -0300

    feat: add PrismaModule import to TransactionsModule
    - Import PrismaModule in TransactionsModule
    - Ensure PrismaService is available for dependency injection
    - Complete module configuration with all required dependencies
    - Enable proper database access for transactions repository

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,9 +3,10 @@ import { TransactionsController } from './transactions.controller';
 import { TransactionProcessingModule } from '../transaction-processing/transaction-processing.module';
 import { TransactionsService } from './transactions.service';
 import { PrismaTransactionsRepository } from './transactions.repository';
+import { PrismaModule } from '../prisma.module';
 
 @Module({
-  imports: [TransactionProcessingModule],
+  imports: [TransactionProcessingModule, PrismaModule],
   controllers: [TransactionsController],
   providers: [TransactionsService, PrismaTransactionsRepository]
 })
